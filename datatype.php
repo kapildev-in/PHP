c2ca58f245868add9e215b92affa27e467e64867
kapildev-in <kapil060398@gmail.com>
2025-03-31 17:38:14 +0530

    Data Types

---
+++ datatype.php
@@ -0,0 +1,30 @@
+<?php
+$name = "Kapil Dev";
+
+// echo var_dump($name);
+
+$num = 10;
+
+// echo var_dump ($num);
+
+$numFloat = 10.10;
+
+// echo var_dump($numFloat);
+
+$bool=true;
+
+// echo var_dump($bool);
+
+
+$arr = ["Kapil", "Anil", 10];
+
+// echo var_dump($arr);
+
+$empty = Null;
+
+// echo var_dump($empty);
+
+// $user = new className();
+
+
+?>
