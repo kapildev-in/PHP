85adc0cc15951e5227b8209bc9f9489a8e50ee45
kapildev-in <kapil060398@gmail.com>
2025-03-31 15:25:17 +0530

    added some comments

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 $variable = "Testing variable";
 $variable_1 = 25;
 $name = "kapil";
-$Name = "Raju";
+$Name = "Raju"; //If N is capital then this variable consider as different in compare to $name
+$a=$b= 45; //can assign same value to multiple variables
 
 echo $variable;
 echo "<br>";
 echo $variable_1 * 5;
 
 echo $name;
+echo "<br>";    
+echo $Name;
+echo "</br>";
+echo $a;
 
 
 ?>
