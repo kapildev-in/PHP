f1b5b965fa56c6fc645a6db5b1e2dfa5bdf3929f
kapildev-in <kapil060398@gmail.com>
2025-03-31 16:24:42 +0530

    PHP Constant

---
+++ constant.php
@@ -0,0 +1,7 @@
+<?php 
+const name= "Kapil"; //use to define a permanent value
+// const name="Ramesh"; you can not assign a second constant if first is already asign, echo gives error. 
+
+echo name; //no need to add any symbol inside it
+
+?>
