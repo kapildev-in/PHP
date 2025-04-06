918b91321f594faeef7fd1dff0f72e3a7455aadc
kapildev-in <kapil060398@gmail.com>
2025-04-06 23:00:35 +0530

    switch statement added

---
+++ switch.php
@@ -0,0 +1,26 @@
+<?php
+ 
+$favoriteColor= "Green";
+
+switch ($favoriteColor) {
+    case "Red":
+        echo "This is my favorite color";
+        break;
+    
+    case "Yellow":
+        echo "This is my brother's favorite color";
+        break;
+
+    case "Green";
+        echo "This is my families favorite color";
+        break;
+
+    default:
+        echo "This color is not matched";
+        break;
+
+
+}
+
+
+?>
